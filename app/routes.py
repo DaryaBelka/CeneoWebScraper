1db46eaac9754f788da038fc28cbfa267f226a20
DaryaBelka <225284@student.uek.krakow.pl>
2023-06-14 01:10:05 +0200

    13.06.2023 DB

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,26 @@
-from flask import Flask,render_template
-import requests
-import json
-import matplotlib
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Darya Belka!"
+    return render_template('index.html')
 
-@app.route('/name/',defaults={'name': "Anonim"})
-@app.route('/name/<name>')
-def name(name):
-    return f"Hello, {name}!"
+@app.route('/extract')
+def extract():
+    return render_template('extract.html')
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('mainpage.html')
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/author')
+def author():
+    return render_template('author.html')
 
 
 
